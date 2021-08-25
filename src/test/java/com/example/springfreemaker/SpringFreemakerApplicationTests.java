9e74e0d75fba5809c5b41a9cc2e3912d0ef58d7d
sunww <sunweiwei@guiji.ai>
2021-08-25 17:20:57 +0800

    mockito测试

--- src/test/java/com/example/springfreemaker/SpringFreemakerApplicationTests.java
+++ src/test/java/com/example/springfreemaker/SpringFreemakerApplicationTests.java
@@ -1,16 +1,26 @@
 package com.example.springfreemaker;
 
+import com.example.springfreemaker.service.SseEmitterService;
+import com.google.common.collect.Lists;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringFreemakerApplicationTests {
 
+    @SpyBean
+    SseEmitterService sseEmitterService;
+
     @Test
     public void contextLoads() {
+        Mockito.when(sseEmitterService.findAll()).thenReturn(Lists.newArrayList("111"));
+        System.out.println(sseEmitterService.findAll());
     }
+
 
 }
